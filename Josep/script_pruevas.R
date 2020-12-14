ac18579719ae3eb5acf6ffaccbd6595c4b21aad7
romanc.josep <romanc.josep@gmail.com>
2020-12-14 21:34:01 +0100

    Ejercicio 7

--- Josep/script_pruevas.R
+++ Josep/script_pruevas.R
@@ -1,6 +1,7 @@
+################################################################################################################
 # 2. Estudia la distribución del numero de películas estrenadas por año. Realiza un gráfico de muestre esta
 # distribución haciendo los ajustes necesarios (agrupaciones, cambios de escala, transformaciones. . . )
-
+################################################################################################################
 scores
 id_year <- group_by(scores, MovieID) %>% 
   summarise(Release_Year = unique(Release_Year)) 
@@ -48,3 +49,43 @@ ggplot(data=movies_per_year, mapping=aes(Release_Year, Count_Movies)) +
   geom_point()
 
 # En estos graficos marcar con una linia los años que se hicieron 2 peliculas o menos o algo así para añadir cositas
+
+
+###############################################################################################################
+# 7. Genera una tabla agrupada por película y año del número de valoraciones.
+# Representa la tabla gráficamente para de las 10 películas con mayor número de valoraciones .
+###############################################################################################################
+
+group_by(scores, MovieID) %>% 
+  summarise(Count_votes = n_distinct(UserID)) 
+
+group_by(scores, year(Date)) %>%
+  summarise(Count_votes = n_distinct(UserID)) 
+
+table7 <- group_by(scores, MovieID) %>% 
+  group_by(Rated_Year=year(Date), add=T) %>% #Add true so that group_by do not overwrite the previous group_by
+  summarise(Count_votes = n_distinct(UserID)) #count users es lo mismo que decir cuantas veces a sido votada.
+#lo mismo
+
+table7 <- group_by(scores, MovieID, Year=year(Date)) %>%
+  summarise(votes = n_distinct((UserID)))
+
+heatdata <- as.data.frame(spread(table7, key=Year, value=votes, fill=0))
+heatdata$total <- rowSums(heatdata[2:8])
+sorted <- arrange(heatdata, desc(total))
+row.names(sorted) <- sorted$MovieID #A una tibble no le puedes especificar nombres a las filas pero a un dataframe si
+heat_m <- as.matrix(select(sorted, -MovieID))
+heatmap(heat_m[,-8], Colv=NA,Rowv=NA,col=rainbow(20))
+
+heatmap(head(heat_m[10:1,-8],10), #pelis ordenadas descencientemente 
+        cexRow=1.5, cexCol=1.5,
+        Colv=NA, Rowv=NA,
+        col=rainbow(25))
+legend(x="left", legend=c(1:20),
+       fill=rainbow(20))
+
+heatmap(head(heat_m[10:1,-8],10), #pelis ordenadas descencientemente 
+        cexRow=1.5, cexCol=1.5,
+        Colv=NA, Rowv=NA,
+        sideColors=c("darkgreen", "yellowgreen"), col=terrain.colors(12))
+#añadir barra de colores.
