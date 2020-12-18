06ac8b69a6ebb7258148f19929d5c8ddb41b55d9
romanc.josep <romanc.josep@gmail.com>
2020-12-18 20:09:14 +0100

    DensityPlot_Ej2

--- Josep/script_pruevas.R
+++ Josep/script_pruevas.R
@@ -16,19 +16,27 @@ summary(movies_per_year)
 #en un 50% de los años se estrenaron como mucho 2 pelis.
 #(tener en cuanta que esto es siempre sobre nuestra 250)
 
-#scatter plot
+##################scatter plot
+
 ggplot(data=movies_per_year) + #sistema de coordenadas al que añadir puntos(creates an empty graph)
-  geom_point(mapping=aes(Release_Year, Count_Movies), color='blue', fill='red', shape=21)  #this is a type of layer you can add.(Scatterplot)
-#if you can add a third class add the aes color(or size or alpha or shape)=third class
+  geom_point(mapping=aes(Release_Year, Count_Movies),color='blue', fill='red', shape=21)  #this is a type of layer you can add.(Scatterplot)
+  #if you can add a third class add the aes color(or size or alpha or shape)=third class
 
 # “The greatest value of a picture is when it forces us to notice what we never expected to see.” — John Tukey
 
-#lineplot con función de densidad
+###################Lineplot con función de densidad
 
 ggplot(data=movies_per_year) + 
   geom_smooth(mapping=aes(Release_Year, Count_Movies)) 
 
-#Barplot
+###################Density
+library(hrbrthemes)
+ggplot(data=id_year, aes(x=Release_Year)) +
+  geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8) +
+  theme_ipsum()
+  
+
+###################Barplot
 
 ggplot(data=id_year) + 
   geom_bar(mapping=aes(x=Release_Year), stat='count', color='blue') # no pide y, le metes una variable y ya te contea por defecto
@@ -49,7 +57,6 @@ ggplot(data=movies_per_year, mapping=aes(Release_Year, Count_Movies)) +
   geom_point()
 
 # En estos graficos marcar con una linia los años que se hicieron 2 peliculas o menos o algo así para añadir cositas
-
 
 ###############################################################################################################
 # 7. Genera una tabla agrupada por película y año del número de valoraciones.
